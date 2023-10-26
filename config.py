3b21532b9e152fe55b26448b5c81c372bb35cce8
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-10-26 17:29:00 +0530

    Update config.py
    Signed-off-by: AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/onenonlybotz/DabangMusic",
+    "https://github.com/onenonlybotz/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
